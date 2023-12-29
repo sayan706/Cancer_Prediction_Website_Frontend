051c831c664ad0778ed85998331302cf9ceb0dd9
sayan706 <sayanchatterjee706@gmail.com>
2023-12-29 13:03:20 +0530

    initial commit by sayan

--- src/components/Lung.jsx
+++ src/components/Lung.jsx
@@ -276,7 +276,7 @@ export default function Lung() {
           onChange={handleData}
           className="w-full px-3 py-2 border rounded-md
                   focus:outline-none focus:border-blue-500"
-          placeholder="Enter age"
+          placeholder="Enter Chest Pain"
           required=""
         />
       </div>
